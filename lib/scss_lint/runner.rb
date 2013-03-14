3436e4006bf6995890b2820b2fd47faefa9f55bc
takumi sato <takumisato0808@gmail.com>
2013-03-14 16:42:54 -0700

    Add support for flag to exclude files
    Adds support for excluding files with the '-e' and '--exclude' switches.
    
    Closes #2
    
    Change-Id: I1a7776a8e73a4617cad6b3d8ef13c4a7cd5aa150
    Reviewed-on: https://gerrit.causes.com/19693
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane@causes.com>

---
+++ lib/scss_lint/runner.rb
@@ -0,0 +1,38 @@
+require 'scss_lint'
+require 'sass'
+
+module SCSSLint
+  class NoFilesError < StandardError; end
+  class NoLintersError < StandardError; end
+
+  class Runner
+    attr_reader :lints
+
+    def initialize
+      @lints = []
+    end
+
+    def run(files = [])
+      raise NoFilesError.new('No SCSS files specified') if files.empty?
+      raise NoLintersError.new('No linters specified') if LinterRegistry.linters.empty?
+
+      files.each do |file|
+        find_lints(file)
+      end
+    end
+
+    def find_lints(file)
+      engine = Engine.new(file)
+
+      LinterRegistry.linters.each do |linter|
+        @lints += linter.run(engine)
+      end
+    rescue Sass::SyntaxError => ex
+      @lints << Lint.new(ex.sass_filename, ex.sass_line, ex.to_s)
+    end
+
+    def lints?
+      @lints.any?
+    end
+  end
+end
