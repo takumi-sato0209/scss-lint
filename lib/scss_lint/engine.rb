b838c36d710113feb87564665bf95a4a19367045
takumi sato <takumisato0808@gmail.com>
2013-05-29 17:05:31 -0700

    Use Engine filename in add_lint
    In preparation for a commit that adds support for traversing
    `Sass::Script::Node`s on top of `Sass::Tree::Node`s, the dependency on
    fetching the filename corresponding to the lint from the node must be
    removed, since `Sass::Script::Node` instances do not have a `filename`
    method.
    
    Since the engine will have the filename passed in, use that instead.
    
    Change-Id: I6040f69e997d59755b608cc6e3d22699f0014f80
    Reviewed-on: https://gerrit.causes.com/23137
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Greg Hurrell <greg@causes.com>

---
+++ lib/scss_lint/engine.rb
@@ -0,0 +1,23 @@
+require 'sass'
+
+module SCSSLint
+  class Engine
+    ENGINE_OPTIONS = { cache: false, syntax: :scss }
+
+    attr_reader :contents, :filename, :lines, :tree
+
+    def initialize(scss_or_filename)
+      if File.exists?(scss_or_filename)
+        @filename = scss_or_filename
+        @engine = Sass::Engine.for_file(scss_or_filename, ENGINE_OPTIONS)
+        @contents = File.open(scss_or_filename, 'r').read
+      else
+        @engine = Sass::Engine.new(scss_or_filename, ENGINE_OPTIONS)
+        @contents = scss_or_filename
+      end
+
+      @lines = @contents.split("\n")
+      @tree = @engine.to_tree
+    end
+  end
+end
