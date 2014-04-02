takumi-sato0209/scss-lint

commit 047f70382d62f6034f6b8b9761076da7e679fb9a

Preserve newlines in Engine#lines
Previously, the `lines` attribute of the `Engine` class would strip
the trailing newline character from each line.

In order to support lines which explicitly check for the presence of a
newline, we should preserve it. This is done by using the handy `lines`
method of the `String` class to break up the string into an array of
strings containing the trailing newlines if they exist.

In the process, updated the implementation of `source_from_range` and
`character_at` to not manually return newlines, since they are now
included by default.

Change-Id: I015c492ef8e5df76accef1cf7eb8a5f565c01418
Reviewed-on: http://gerrit.causes.com/36832
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane@causes.com>

lib/scss_lint/engine.rb
@@ -20,7 +20,7 @@ module SCSSLint
         @contents = scss_or_filename
       end
 
-      @lines = @contents.split("\n")
+      @lines = @contents.lines
       @tree = @engine.to_tree
     rescue Encoding::UndefinedConversionError, ArgumentError => error
       if error.is_a?(Encoding::UndefinedConversionError) ||

spec/scss_lint/engine_spec.rb
@@ -17,8 +17,8 @@ describe SCSSLint::Engine do
   context 'when the file being linted has an invalid byte sequence' do
     let(:css) { "\xC0\u0001" }
 
-    it 'raises a FileEncodingError' do
-      expect { engine }.to raise_error(SCSSLint::FileEncodingError)
+    it 'raises a SyntaxError' do
+      expect { engine }.to raise_error(Sass::SyntaxError)
     end
   end
 end

lib/scss_lint/linter.rb
@@ -37,9 +37,6 @@ module SCSSLint
       actual_line   = source_position.line - 1
       actual_offset = source_position.offset + offset - 1
 
-      # Return a newline if offset points at the very end of the line
-      return "\n" if actual_offset == engine.lines[actual_line].length
-
       engine.lines[actual_line][actual_offset]
     end
 
@@ -60,7 +57,7 @@ module SCSSLint
 
       current_line += 1
       while current_line < last_line
-        source += "#{engine.lines[current_line]}\n"
+        source += "#{engine.lines[current_line]}"
         current_line += 1
       end
 
@@ -68,7 +65,7 @@ module SCSSLint
          # Sometimes the parser reports ranges ending on the first column of the
          # line after the last line; don't include the last line in this case.
          engine.lines.count == current_line - 1
-        source += "#{engine.lines[current_line][0...source_range.end_pos.offset]}\n"
+        source += "#{engine.lines[current_line][0...source_range.end_pos.offset]}"
       end
 
       source

commit 659de73e22fa54d56db401d785d740e3ab1961a2

Fix broken specs for Ruby 1.9.3
Commit 047f703 introduced a change that broke the specs for 1.9.3.

The solution was to add a `.to_a` to the call to `lines` so that an
array is always returned.

Change-Id: I880d16701d27e98161b7f075ebb66e77b45f1836
Reviewed-on: http://gerrit.causes.com/36835
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane@causes.com>

lib/scss_lint/engine.rb
@@ -20,11 +20,11 @@ module SCSSLint
         @contents = scss_or_filename
       end
 
-      @lines = @contents.lines
+      @lines = @contents.lines.to_a # Need `to_a` for Ruby 1.9.3
       @tree = @engine.to_tree
-    rescue Encoding::UndefinedConversionError, ArgumentError => error
+    rescue Encoding::UndefinedConversionError, Sass::SyntaxError => error
       if error.is_a?(Encoding::UndefinedConversionError) ||
-         error.message.include?('invalid byte sequence')
+         error.message.match(/invalid.*(byte sequence|character)/i)
         raise FileEncodingError,
               "Unable to parse SCSS file: #{error}",
               error.backtrace

spec/scss_lint/engine_spec.rb
@@ -18,7 +18,7 @@ describe SCSSLint::Engine do
     let(:css) { "\xC0\u0001" }
 
     it 'raises a SyntaxError' do
-      expect { engine }.to raise_error(Sass::SyntaxError)
+      expect { engine }.to raise_error(SCSSLint::FileEncodingError)
     end
   end
 end