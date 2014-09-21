takumi-sato0209/scss-lint

commit b7637583015650aa93f7319de2b8993b9e5a98b2

Fix crash in LeadingZero
Due to an upstream Sass bug (https://github.com/sass/sass/issues/1343),
check for the case where a source range is incorrect and pass if it is.

This means we may not report some lints, but this case should be
relatively rare it's better than crashing.

Change-Id: I079e6a6681400ed574a9dda035c9555748188ee1
Reviewed-on: http://gerrit.causes.com/41017
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

lib/scss_lint/linter/leading_zero.rb
@@ -14,7 +14,10 @@ module SCSSLint
     end
 
     def visit_script_number(node)
-      return unless number = source_from_range(node.source_range)[FRACTIONAL_DIGIT_REGEX, 1]
+      # TODO: Remove once https://github.com/sass/sass/issues/1343 is fixed
+      return unless source = source_from_range(node.source_range)
+
+      return unless number = source[FRACTIONAL_DIGIT_REGEX, 1]
 
       check_number(node, number)
     end

commit 997c3a3c3bb8c1e29adf1d69056a91033a9c4388

Remove temporary workaround for sass issue #1343
This was introduced in b7637583 in order to work around a bug in the
Sass parser that would generate an incorrect source map for interpolated
expressions spanning multiple lines.

Now that the problem has been fixed upstream, we can remove it.

Change-Id: I6f90821d105b7c7f1e7a11bb79a8c6f99fd54e81
Reviewed-on: http://gerrit.causes.com/42832
Tested-by: jenkins <jenkins@causes.com>
Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

lib/scss_lint/linter/leading_zero.rb
@@ -14,10 +14,8 @@ module SCSSLint
     end
 
     def visit_script_number(node)
-      # TODO: Remove once https://github.com/sass/sass/issues/1343 is fixed
-      return unless source = source_from_range(node.source_range)
-
-      return unless number = source[FRACTIONAL_DIGIT_REGEX, 1]
+      return unless number =
+        source_from_range(node.source_range)[FRACTIONAL_DIGIT_REGEX, 1]
 
       check_number(node, number)
     end