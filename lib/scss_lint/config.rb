takumi-sato0209/scss-lint

commit f5be4306d78690f2e8903025b44bc5d8835a6374

Respect scss_files option in Rake tasks
The way we had implemented the FileFinder caused us to not respect the
`scss_files` config option when running SCSS-Lint via the Rake task
integration.

Work around this by passing in `scss_files` to the FileFinder rather
than have the finder know of its existence.

Fixes #726

lib/scss_lint/config.rb
@@ -304,7 +304,7 @@ module SCSSLint
 
     # @return Array
     def scss_files
-      if path = @options['scss_files']
+      if path = @options['scss_files'] && Array(path).any?
         Array(path).map { |p| Dir[p] }.flatten.uniq
       else
         []

lib/scss_lint/cli.rb
@@ -63,14 +63,7 @@ module SCSSLint
 
     def scan_for_lints(options, config)
       runner = Runner.new(config)
-      files =
-        if options[:stdin_file_path]
-          [{ file: STDIN, path: options[:stdin_file_path] }]
-        else
-          FileFinder.new(config).find(options[:files]).map do |file_path|
-            { path: file_path }
-          end
-        end
+      files = files_to_lint(options, config)
       runner.run(files)
       report_lints(options, runner.lints, files)
 
@@ -80,6 +73,17 @@ module SCSSLint
         halt :warning
       else
         halt :ok
+      end
+    end
+
+    def files_to_lint(options, config)
+      if options[:stdin_file_path]
+        [{ file: STDIN, path: options[:stdin_file_path] }]
+      else
+        patterns = Array(options[:files]).any? ? Array(options[:files]) : config.scss_files
+        FileFinder.new(config).find(patterns).map do |file_path|
+          { path: file_path }
+        end
       end
     end
 

lib/scss_lint/file_finder.rb
@@ -19,8 +19,10 @@ module SCSSLint
     #
     # @param patterns [Array<String>] a list of file paths and glob patterns
     def find(patterns)
-      # If no explicit patterns given, use patterns listed in config
-      patterns = @config.scss_files if patterns.empty?
+      if patterns.empty?
+        raise SCSSLint::Exceptions::NoFilesError,
+              'No files, paths, or patterns were specified'
+      end
 
       matched_files = extract_files_from(patterns)
       if matched_files.empty?

lib/scss_lint/rake_task.rb
@@ -50,7 +50,7 @@ module SCSSLint
     # Create the task so it is accessible via +Rake::Task['scss_lint']+.
     def initialize(name = :scss_lint)
       @name = name
-      @files = ['.'] # Search for everything under current directory by default
+      @files = []
       @quiet = false
 
       yield self if block_given?
@@ -102,7 +102,13 @@ module SCSSLint
       # command line or in a custom task definition.
       explicit_files = Array(task_args[:files]) + Array(task_args.extras)
 
-      explicit_files.any? ? explicit_files : files
+      if explicit_files.any?
+        explicit_files
+      elsif files.any?
+        files
+      else
+        [] # Will fall back to scss_files option if defined
+      end
     end
 
     # Friendly description that shows the full command that will be executed.

spec/scss_lint/file_finder_spec.rb
@@ -14,30 +14,8 @@ describe SCSSLint::FileFinder do
     context 'when no patterns are given' do
       let(:patterns) { [] }
 
-      context 'and there are no SCSS files under the current directory' do
-        it 'raises an error' do
-          expect { subject }.to raise_error SCSSLint::Exceptions::NoFilesError
-        end
-      end
-
-      context 'and there are SCSS files under the current directory' do
-        before do
-          FileUtils.touch('blah.scss')
-          FileUtils.mkdir_p('more')
-          FileUtils.touch(File.join('more', 'more.scss'))
-        end
-
-        it { should == ['blah.scss', File.join('more', 'more.scss')] }
-      end
-
-      context 'and a default set of files is specified in the config' do
-        let(:files) { ['file1.scss', 'file2.scss'] }
-
-        before do
-          config.stub(:scss_files).and_return(files)
-        end
-
-        it { should == files }
+      it 'raises an error' do
+        expect { subject }.to raise_error SCSSLint::Exceptions::NoFilesError
       end
     end
 

commit c9eb2fe01919810647aa00d183995d60bef8e8b7

Fix typo in Config#scss_files
This was introduced in f5be430.

lib/scss_lint/config.rb
@@ -304,7 +304,7 @@ module SCSSLint
 
     # @return Array
     def scss_files
-      if path = @options['scss_files'] && Array(path).any?
+      if (path = @options['scss_files']) && Array(path).any?
         Array(path).map { |p| Dir[p] }.flatten.uniq
       else
         []